c9ecaf231dcd43850d4bbf85769cbf43e699da8d
ricardogk3 <rgk19@hotmail.com>
2021-08-26 13:33:50 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp();
+}
+
+
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'Supreme'
+    );
+  }
+}
+
